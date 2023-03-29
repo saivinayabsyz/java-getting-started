d023500605f797a5211fdb097fb337dd1f518af9
saivinayabsyz <30690019+saivinayabsyz@users.noreply.github.com>
2023-03-30 01:23:47 +0530

    Create AuthParams.java

---
+++ src/main/java/com/heroku/java/AuthParams.java
@@ -0,0 +1,38 @@
+package com.example;
+
+import javax.persistence.*;
+import javax.xml.bind.annotation.*;
+
+/*
+ * a simple domain entity doubling as a DTO
+ */
+@Entity
+@Table(name = "authParams")
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class AuthParams {
+
+  @Column()
+    String accessToken;
+	@Column()
+    String orgURL;
+	@Column()
+    String userID;
+	@Column()
+    String fromDate;
+	@Column()
+    String toDate;
+
+   
+    public AuthParams() {
+    }
+
+    public AuthParams(String accessToken, String orgURL, String userID,String fromDate,String toDate) {
+        this.accessToken = accessToken;
+        this.orgURL = orgURL;
+        this.userID = userID;
+		 this.fromDate = fromDate;
+        this.toDate = toDate;
+    }
+    
+}
